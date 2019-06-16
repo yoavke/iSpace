ea07376f39b54143437d1c9f9ed69311740cb005
Ashpigelman <Ashpigelman@gmail.com>
2019-06-16 22:13:57 +0300

    first commit

---
+++ app/src/main/java/com/hit/ispace/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hit.ispace;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/hit/ispace/MainMenu.java
@@ -0,0 +1,15 @@
+package com.hit.ispace;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
