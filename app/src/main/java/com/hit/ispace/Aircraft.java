e6a6f510c90fafdfec4b682a53e4782b39f29597
Yoav Keren <yoavke@gmail.com>
2019-06-22 14:20:05 +0300

    add distance and coordinate

--- app/src/main/java/com/hit/ispace/Aircraft.java
+++ app/src/main/java/com/hit/ispace/Aircraft.java
@@ -4,10 +4,15 @@ package com.hit.ispace;
 public final class Aircraft {
     private String name;
     private String image;
+    private int xCoordinate;
+    private int distanceCovered;
 
     public Aircraft() {
         //TODO: get my collection of aircraft
         // from DB and set the name and image
+
+         this.xCoordinate = 1;
+         this.distanceCovered = 0;
     }
 
     public String getName() {
@@ -24,5 +29,21 @@ public final class Aircraft {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getDistanceCovered() {
+        return distanceCovered;
+    }
+
+    public void setDistanceCovered(int distanceCovered) {
+        this.distanceCovered = distanceCovered;
     }
 }
