9c59a60537e0b4e2718d7f4045b104ccd2becc2a
Yoav Keren <yoavke@gmail.com>
2019-06-25 17:39:25 +0300

    update LevelActivity and settings in Settings class

--- app/src/main/java/com/hit/ispace/LevelActivity.java
+++ app/src/main/java/com/hit/ispace/LevelActivity.java
@@ -2,8 +2,11 @@ package com.hit.ispace;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class LevelActivity extends AppCompatActivity {
+
+    private static final String TAG = LevelActivity.class.getSimpleName();
 
     private Level level;
 
@@ -15,13 +18,28 @@ public class LevelActivity extends AppCompatActivity {
         //get the view of the level
         LevelView levelView = findViewById(R.id.levelview);
 
-        //play 5 levels
-        for (int i=1; i<=5; i++) {
-            level = new Level(i);
-            levelView.startLevel(level);
+        int levelType = getIntent().getIntExtra("levelType", 0);
 
-            //loop until the game ends (finished or failed)
-            while (!level.isLevelEnded()) {}
+        //check if the level type chosen exists
+        switch (levelType) {
+            case Settings.LevelTypes.FREE_STYLE:
+            case Settings.LevelTypes.GETTING_SICK:
+                //instantiate level here so we dont instantiate it for level that doesn't exists
+                level = new Level(levelType);
+                levelView.startLevel(level);
+            default:
+                Log.e(TAG, "No such level type");
+
+                //finish activity with relevant request_code
+                finishActivity(Settings.RequestCodes.LEVEL_NOT_EXIST);
+
         }
+
+
+        //loop until the game ends
+        while (!level.isLevelEnded()) {}
+
+        //finish activity with relevant request_code
+        finishActivity(Settings.RequestCodes.GAME_ENDED);
     }
 }
